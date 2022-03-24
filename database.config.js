9a97d325add5c85ff788034d2fc8713af571f6b4
VirtualCreations0 <101089644+VirtualCreations0@users.noreply.github.com>
2022-03-25 02:23:27 +0500

    Add files via upload

---
+++ database.config.js
@@ -0,0 +1,8 @@
+/** @format */
+
+module.exports = {
+	    url: "mongodb+srv://rashid:rashid1222@cluster0.9wejn.mongodb.net/crudDb?retryWrites=true&w=majority",
+
+    // url: "mongodb+srv://richmonkeystaking:richmonkeystaking@cluster0.udxxd.mongodb.net/richmonkeystaking?retryWrites=true&w=majority",
+
+};
